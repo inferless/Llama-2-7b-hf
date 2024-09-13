7495e07d7c6d01c862a1ee5584c12f0e305e8f13
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-13 19:51:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,86 +1,32 @@
-from threading import Thread
-from typing import Iterator
-import os
-
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
-
-model_id = 'meta-llama/Llama-2-7b-chat-hf'
 
 class InferlessPythonModel:
-    def get_prompt(self, message, chat_history,
-               system_prompt):
-        texts = [f'[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n']
-        for user_input, response in chat_history:
-            texts.append(f'{user_input.strip()} [/INST] {response.strip()} </s><s> [INST] ')
-        texts.append(f'{message.strip()} [/INST]')
-        return ''.join(texts)
-
-    
-    def get_input_token_length(self, message, chat_history, system_prompt):
-        prompt = self.get_prompt(message, chat_history, system_prompt)
-        input_ids = self.tokenizer([prompt], return_tensors='np')['input_ids']
-        return input_ids.shape[-1]
-
-
-    def run_function(self, message,
-        chat_history,
-        system_prompt,
-        max_new_tokens=1024,
-        temperature=0.8,
-        top_p=0.95,
-        top_k=5):
-        prompt = self.get_prompt(message, chat_history, system_prompt)
-        inputs = self.tokenizer([prompt], return_tensors='pt').to('cuda')
-
-        streamer = TextIteratorStreamer(self.tokenizer,
-                                        timeout=10.,
-                                        skip_prompt=True,
-                                        skip_special_tokens=True)
-        generate_kwargs = dict(
-            inputs,
-            streamer=streamer,
-            max_new_tokens=max_new_tokens,
-            do_sample=True,
-            top_p=top_p,
-            top_k=top_k,
-            temperature=temperature,
-            num_beams=1,
-        )
-        t = Thread(target=self.model.generate, kwargs=generate_kwargs)
-        t.start()
-
-        outputs = ''
-        for text in streamer:
-            outputs += text
-
-        return outputs
-
-
     def initialize(self):
-        token = "<Your HF Token >"
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=token)
-        if torch.cuda.is_available():
-            self.model = AutoModelForCausalLM.from_pretrained(
-                model_id,
-                torch_dtype=torch.float16,
-                device_map='auto',
-                use_auth_token=token
-            )
-        else:
-            self.model = None
+        self.tokenizer = AutoTokenizer.from_pretrained("NousResearch/Llama-2-13b-chat-hf", use_fast=True)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            "NousResearch/Llama-2-13b-chat-hf",
+            torch_dtype=torch.float16,
+            device_map="cuda"
+        )
 
     def infer(self, inputs):
-        message = inputs['message']
-        chat_history = inputs['chat_history'] if 'chat_history' in inputs else []
-        system_prompt = inputs['system_prompt'] if 'system_prompt' in inputs else ''
-        result = self.run_function(
-            message=message,
-            chat_history=chat_history,
-            system_prompt=system_prompt,
-        )
-        return {"generated_text": result}
+        prompt = inputs["prompt"]
+        input_ids = self.tokenizer(prompt, return_tensors='pt').input_ids.to(self.model.device)
+        
+        with torch.no_grad():
+            output = self.model.generate(
+                inputs=input_ids,
+                temperature=0.7,
+                max_new_tokens=512,
+                do_sample=True,
+                pad_token_id=self.tokenizer.eos_token_id
+            )
+        
+        result = self.tokenizer.decode(output[0], skip_special_tokens=True)
+        return {"generated_result": result}
 
     def finalize(self):
-        self.tokenizer = None
         self.model = None
+        self.tokenizer = None
+        torch.cuda.empty_cache()
